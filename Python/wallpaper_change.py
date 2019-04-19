fc48d0447bc8521f669bbee88c8593f8b3638fc7
sarthak567 <samantaray.subhasarthak@gmail.com>
2019-04-19 12:56:10 +0530

    Python program to change desktop wallpaper
    Python program to change desktop wallpaper in windows 10 PC

---
+++ Python/wallpaper_change.py
@@ -0,0 +1,29 @@
+import ctypes
+import os 
+import random 
+import time
+import schedule
+
+def is_img_file(f):
+    return f.endswith('jpg') or f.endswith('png') or f.endswith('jpeg')
+
+def job():
+    wallpaper_dir = "D:\\Sarthak_S\\Wallpapers"
+
+    img_files = os.listdir(wallpaper_dir)
+    img_files = list(filter(is_img_file, img_files))
+    # print("Img Files: {}".format(img_files))
+
+    rand_num = random.randint(0, len(img_files)-1)
+    desktop_wallpaper_img = wallpaper_dir + "\\" + str(img_files[rand_num])
+
+    # print("Desktop Wallpaper:{}".format(desktop_wallpaper_img))
+
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, desktop_wallpaper_img, 0)
+
+schedule.every().hour.do(job)
+
+if __name__ == '__main__':
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
